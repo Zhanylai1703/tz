526a6c28a12a4f9cb9aa122784207f57325923b5
Zhanylai <kasymkulovajanylai@gmail.com>
2023-01-27 01:13:17 +0600

    points

--- main/settings.py
+++ main/settings.py
@@ -82,12 +82,18 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'postgres',
+        'USER': 'Zhan',
+        'PASSWORD': 'mypassword1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import CustomUserSerializer
 
 # Create your views here.
+
+class CustomUserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
